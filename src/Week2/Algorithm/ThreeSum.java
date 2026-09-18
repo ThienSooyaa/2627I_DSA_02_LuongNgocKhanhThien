0951ce7d82d352dc0313f06b8bd0ee0fd25f9c33
NITRO <yeuchijisoo2007@gmail.com>
2026-09-18 23:41:00 +0700

    the first two excercises of week 2

---
+++ src/Week2/Algorithm/ThreeSum.java
@@ -0,0 +1,66 @@
+package Week2.Algorithm;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class ThreeSum {
+
+    // Do not instantiate.
+    private ThreeSum() { }
+
+    /**
+     * Prints to standard output the (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param a the array of integers
+     */
+    public static void printAll(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    // Ép kiểu sang long để tránh tràn số khi cộng 3 số int
+                    if ((long) a[i] + a[j] + a[k] == 0) {
+                        StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the number of triples (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param  a the array of integers
+     * @return the number of triples (i, j, k) with {@code i < j < k}
+     *         such that {@code a[i] + a[j] + a[k] == 0}
+     */
+    public static int count(int[] a) {
+        int n = a.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    // Ép kiểu sang long để tránh tràn số khi cộng 3 số int
+                    if ((long) a[i] + a[j] + a[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args)  {
+        In in = new In(args[0]);
+        int[] a = in.readAllInts();
+
+        Stopwatch timer = new Stopwatch();
+        int count = count(a);
+        StdOut.println("elapsed time = " + timer.elapsedTime());
+        StdOut.println(count);
+    }
+}
+
